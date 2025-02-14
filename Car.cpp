15b706e9e7a687937ce6f5177ded852253bc7fc0
ljg7234 <ljg7234@naver.com>
2025-02-14 23:09:44 +0900

    First commit

---
+++ Car.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include "Car.h"
+using namespace std;
+
+void Car::InitMembers(int fuel){
+    fuelGauge=fuel;
+    curSpeed =0;
+}
+
+void Car::ShowCarState(){
+    cout<<"연료량: "<<fuelGauge<<"%"<<endl;
+    cout<<"현재 속도: "<<curSpeed<<endl<<endl;
+}
+
+void Car::Accel(){
+    if(fuelGauge <=0){
+        return;
+    } else {
+        fuelGauge -= CAR_CONST::FUEL_STEP;
+    }
+    if(curSpeed+CAR_CONST::ACC_STEP>=CAR_CONST::MAX_SPD){
+        curSpeed = CAR_CONST:: MAX_SPD;
+        return ;
+    }
+
+    curSpeed += CAR_CONST:: ACC_STEP;
+}
+
+void Car::Break(){
+    if(curSpeed <CAR_CONST:: BRK_STEP){
+        curSpeed = 0;
+        return;
+    }
+    curSpeed -= CAR_CONST::BRK_STEP;
+}
---
+++ Rectangle.h
@@ -0,0 +1,12 @@
+#ifndef __RECTANGLE_H_
+#define __RECTANGLE_H_
+#include "Point.h"
+class Rectangle{
+    private:
+        Point upleft;
+        Point lowRight;
+    public:
+        Rectangle(const int &x1, const int &y1, const int &x2, const int &y2);       
+        void ShowRecInfo() const;
+};
+#endif
